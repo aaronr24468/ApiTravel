929200a8741009b8d6b6dec57956f0af7d9339f1
aaronr24468 <aaronr24468@gmail.com>
2026-03-28 16:43:52 -0600

    CreatePool configuration

--- src/connectionDB/connection.mjs
+++ src/connectionDB/connection.mjs
@@ -2,9 +2,14 @@ import mysql from 'mysql2/promise'
 import {config} from 'dotenv';
 config();
 
-export const connectionDB = await mysql.createConnection({
+export const connectionDB = await mysql.createPool({
     host: `${process.env.HOST}`,
     user: `${process.env.USER}`,
     password: `${process.env.PASSWORD}`,
-    database: `${process.env.DB}`
+    database: `${process.env.DB}`,
+
+    waitForConnections: true,
+    connectionLimit: 5,
+    queueLimit: 0,
+    connectTimeout: 10000
 })
